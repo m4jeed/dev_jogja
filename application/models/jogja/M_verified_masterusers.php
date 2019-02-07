83c45742b2609b01cc519e0ce9bc8b00078dabf1
m4jeed <majeedprogrammerweb@gmail.com>
2019-02-07 15:14:36 +0700

    update models verified

--- application/models/jogja/M_verified_masterusers.php
+++ application/models/jogja/M_verified_masterusers.php
@@ -20,10 +20,13 @@ class M_verified_masterusers extends CI_Model{
 		$sql="SELECT ma_users.*,
 			ma_vacc.vacc_number,
 			ma_vacc.balance,
-			ma_vacc.poin 
+			ma_vacc.poin,
+			ma_trx.vacc_number 
 			FROM ma_users
 			INNER JOIN ma_vacc
 			ON ma_users.user_id=ma_vacc.user_id
+			INNER JOIN ma_trx
+			ON ma_users.user_id=ma_trx.trx_id
 			WHERE is_active='1'
 			AND is_verified='1'
 			AND (fullname LIKE '%".$keyword."%'
@@ -42,6 +45,8 @@ class M_verified_masterusers extends CI_Model{
 			FROM ma_users
 			INNER JOIN ma_vacc
 			ON ma_users.user_id=ma_vacc.user_id
+			INNER JOIN ma_trx
+			ON ma_users.user_id=ma_trx.trx_id
 			WHERE is_active='1'
 			AND is_verified='1'
 			AND (fullname LIKE '%".$keyword."%'
@@ -50,4 +55,41 @@ class M_verified_masterusers extends CI_Model{
 		return $this->db->query($sql)->row()->jumlah;
 	}
 
+	function get_one($user_id){
+		$sql="SELECT ma_users.*,
+			ma_vacc.vacc_number,
+			ma_vacc.balance,
+			ma_vacc.poin,
+			base_postalcode.city,
+			base_postalcode.districts,
+			base_postalcode.postalcode,
+			base_postalcode.province,
+			base_postalcode.village
+			FROM ma_users
+			INNER JOIN ma_vacc
+			ON ma_users.user_id=ma_vacc.user_id
+			LEFT JOIN base_postalcode
+			ON ma_users.postalcode_id=base_postalcode.ids
+			WHERE ma_users.user_id='".$user_id."'";
+			//var_dump($sql);die();
+		return $this->db->query($sql)->row_array();
+	}
+
+	function get_one_vacc($vacc){
+		$sql="SELECT ma_trx.*,
+			ma_vacc.user_id,
+			ma_users.user_id,
+			ma_users.fullname
+			FROM ma_trx
+			INNER JOIN ma_vacc
+			ON ma_trx.vacc_number=ma_vacc.vacc_number
+			INNER JOIN ma_users
+			ON ma_trx.trx_id=ma_users.user_id
+			WHERE ma_trx.vacc_number='".$vacc."' 
+			GROUP BY trx_date DESC";
+		return $this->db->query($sql)->result_array();
+	}
+
 }
+
+/*update*/
